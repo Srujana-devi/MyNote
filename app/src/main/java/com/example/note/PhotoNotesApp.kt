eb68c069a4e3a7c7d68070c6344afc4841bbb572
Srujana Killi <srujanadevi429@gmail.com>
2023-11-17 11:22:59 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/note/PhotoNotesApp.kt
@@ -0,0 +1,51 @@
+package com.example.note
+
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.room.Room
+import com.example.note.persistence.NotesDao
+import com.example.note.persistence.NotesDatabase
+
+class PhotoNotesApp : Application(){
+
+    private var db : NotesDatabase? = null
+
+
+    init {
+        instance = this
+    }
+
+    private fun getDb(): NotesDatabase {
+        return if (db != null){
+            db!!
+        } else {
+            db = Room.databaseBuilder(
+                instance!!.applicationContext,
+                NotesDatabase::class.java, Constants.DATABASE_NAME
+            ).fallbackToDestructiveMigration()// remove in prod
+                .build()
+            db!!
+        }
+    }
+
+
+    companion object {
+        private var instance: PhotoNotesApp? = null
+
+        fun getDao(): NotesDao {
+            return instance!!.getDb().NotesDao()
+        }
+
+        fun getUriPermission(uri: Uri){
+            instance!!.applicationContext.contentResolver.takePersistableUriPermission(
+                uri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+        }
+
+    }
+
+
+}
